557fc58e18d61a082b31bc5d724251cafdcd1c49
Sahetmyrat <kakajansahy@gmail.com>
2024-12-15 23:36:34 +0200

    creating News App backend

---
+++ src/test/java/com/sahet/newsbackend/NewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahet.newsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
